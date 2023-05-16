7b3e140ee3a242a3c13633c8f8ca954714159d0f
Agota Hanasz <agota.hanasz.1990@icloud.com>
2023-05-16 09:31:30 +0200

    added CSS

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const nameInput = document.getElementById('name');
-const raceSelect = document.getElementById('race');
-const classSelect = document.getElementById('class');
+// const nameInput = document.getElementById('name');
+// const raceSelect = document.getElementById('race');
+// const classSelect = document.getElementById('class');
 
-const csName = document.getElementById('cs-name');
-const csRace = document.getElementById('cs-race');
-const csClass = document.getElementById('cs-class');
+// const csName = document.getElementById('cs-name');
+// const csRace = document.getElementById('cs-race');
+// const csClass = document.getElementById('cs-class');
 
-nameInput.addEventListener('input', updateCharacterSheet);
-raceSelect.addEventListener('input', updateCharacterSheet);
-classSelect.addEventListener('input', updateCharacterSheet);
+// nameInput.addEventListener('input', updateCharacterSheet);
+// raceSelect.addEventListener('input', updateCharacterSheet);
+// classSelect.addEventListener('input', updateCharacterSheet);
 
-function updateCharacterSheet() {
-  csName.innerText = nameInput.value;
-  csRace.innerText = raceSelect.options[raceSelect.selectedIndex].text;
-  csClass.innerText = classSelect.options[classSelect.selectedIndex].text;
-}
+// function updateCharacterSheet() {
+//   csName.innerText = nameInput.value;
+//   csRace.innerText = raceSelect.options[raceSelect.selectedIndex].text;
+//   csClass.innerText = classSelect.options[classSelect.selectedIndex].text;
+// }
