0d3af488c87a5eb6f3215091d5d6502a6ca94cd4
Agota Hanasz <agota.hanasz.1990@icloud.com>
2023-05-17 07:21:49 +0200

    Working on Characters

--- script.js
+++ script.js
@@ -15,3 +15,32 @@
 //   csRace.innerText = raceSelect.options[raceSelect.selectedIndex].text;
 //   csClass.innerText = classSelect.options[classSelect.selectedIndex].text;
 // }
+
+//     const root = document.getElementById('root');
+//     const container = document.createElement('div');
+//     container.classList.add('container');
+//     root.append(container);
+//     let sec = document.createElement('section');
+//     sec.classList.add('main');
+//     container.append(sec);
+
+//     document.addEventListener('DOMContentLoaded', function() {
+//     const form = document.querySelector('form');
+//     form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+  
+      
+//       const name = document.getElementById('name').value;
+//       const gender = document.getElementById('gender').value;
+//       const race = document.getElementById('race').value;
+//       const classValue = document.getElementById('class').value;
+  
+//       console.log('Name:', name);
+//       console.log('Gender:', gender);
+//       console.log('Race:', race);
+//       console.log('Class:', classValue);
+      
+//       form.reset();
+//     });
+//   });
+
